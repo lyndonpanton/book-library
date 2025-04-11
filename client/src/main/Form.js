a135f4e9be620b6e3350b11f41f3fe9bc145c39c
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-11 23:44:32 +0100

    Users is only allowed to submit a new book if form data is valid

--- client/src/main/Form.js
+++ client/src/main/Form.js
@@ -14,27 +14,93 @@ function Form() {
     function submitNewBook(e) {
         e.preventDefault();
 
-        // Error handling (see database schema, isEmpty and isFormattedCorrectly)...
+        let validISBN = false;
+        let validTitle = false;
+        let validAuthor = false;
+        let validReleaseYear = false;
+        let validCoverURL = false;
+        let validTotalPages = false;
 
-        // axios.post("http://localhost:3001/api/insert", {
-        //     isbn: newISBN,
-        //     title: newTitle,
-        //     author: newAuthor,
-        //     release_year: newReleaseYear,
-        //     coverURL: newCoverURL,
-        //     totalPages: newTotalPages
-        // });
+        // Validation (see database schema, isEmpty and isFormattedCorrectly)...
+        if (newISBN.length == 13) {
+            validISBN = true;
+        }
 
-        // Update UI
+        if (newTitle.length > 0) {
+            validTitle = true;
+        }
 
-        // Reset Form and State
-        setNewISBN("");
-        setNewTitle("");
-        setNewAuthor("");
-        setNewReleaseYear("");
-        setNewCoverURL("");
-        setNewTotalPages("");
+        if (newAuthor.length > 2) {
+            validAuthor = true;
+        }
+
+        if (newReleaseYear.length == 4 && !isNaN(newReleaseYear)) {
+
+        }
+
+        // Valid URL as well as length...
+        if (newCoverURL.length > 0) {
+            validCoverURL = true;
+        }
+
+        if (newTotalPages.length > 5) {
+            validTotalPages = true;
+        }
         
+        // Highlight fields with the incorrect format on submission
+
+        // Database SQL Injection validation?
+
+        if (
+                validISBN && validTitle && validAuthor && validReleaseYear
+                && validCoverURL && validTotalPages
+        ) {
+            // axios.post("http://localhost:3001/api/insert", {
+            //         isbn: newISBN,
+            //         title: newTitle,
+            //         author: newAuthor,
+            //         release_year: newReleaseYear,
+            //         coverURL: newCoverURL,
+            //         totalPages: newTotalPages
+            // });
+
+            // Update UI
+
+            // Reset Form and State
+            setNewISBN("");
+            setNewTitle("");
+            setNewAuthor("");
+            setNewReleaseYear("");
+            setNewCoverURL("");
+            setNewTotalPages("");
+        } else {
+            if (!validISBN) {
+                console.log(
+                    "ISBN must be 13 digits long and only contain numbers"
+                );
+            }
+
+            if (!validTitle) {
+                console.log("Invalid book title");
+            }
+
+            if (!validAuthor) {
+                console.log("Invalid book author");
+
+            }
+
+            if (!validReleaseYear) {
+                console.log("Invalid book release year");
+            }
+
+            if (!validCoverURL) {
+                console.log("Invalid book cover URL");
+            }
+
+            if (!validTotalPages) {
+                console.log("Invalid book total pages");
+            }
+        }
     }
 
     return (
