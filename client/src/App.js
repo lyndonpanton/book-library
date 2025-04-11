a7f16c9d8e8f83dd9166015b932523bf1e6c16fd
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-11 20:14:27 +0100

    Added state variables for new book form fields

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+    // New book: isbn, title, author, release_year, cover_url, total_pages
+    const [newISBN, getNewISBN] = useState(0);
+    const [newTitle, getNewTitle] = useState("");
+    const [newAuthor, getNewAuthor] = useState("");
+    const [newReleaseYear, getNewReleaseYear] = useState(0);
+    const [newCoverURL, getNewCoverURL] = useState("");
+    const [newTotalPages, getNewTotalPages] = useState(0);
+
+    const [books, setBooks] = useState([]);
+
+    function submitNewBook(e) {
+        e.preventDefault();
+    }
+
+    // Update state variables
     return (
         <div className="App">
             <header>
                 <h1>Book Library</h1>
             </header>
             <main>
+                <form id="book-form" onSubmit={ submitNewBook }>
+                    <label></label>
+                    <input type="text "></input>
+                </form>
+                <section id="library">
+                {
+                    books.map(function (book, index) {
 
+                    })
+                }
+                </section>
             </main>
             <footer>
                 
