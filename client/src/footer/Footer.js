38c4eb6cb1267f513e8d43360d50fb035834dc0a
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-12 18:39:02 +0100

    Added footer content

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -2,7 +2,53 @@ function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <p id="copyright">
+            {/* General, Tech Stack, Applications */}
+            <section id="footer-main-links">
+                <ul className="footer-main-link-list">
+                    {/* Developer */}
+                    <li className="footer-main-link">About Me</li>
+                    <li className="footer-main-link">Website</li>
+                    <li className="footer-main-link">GitHub</li>
+                    <li className="footer-main-link">LinkedIn</li>
+                </ul>
+                <ul className="footer-main-link-list">
+                    {/* Languages and frameworks */}
+                    <li className="footer-main-link">JavaScript</li>
+                    <li className="footer-main-link">ReactJS</li>
+                    <li className="footer-main-link">CSS</li>
+                    <li className="footer-main-link">NodeJS</li>
+                    <li className="footer-main-link">MySQL</li>
+                </ul>
+                <ul className="footer-main-link-list">
+                    {/* Packages */}
+                    <li className="footer-main-link">axios</li>
+                    <li className="footer-main-link">body-parser</li>
+                    <li className="footer-main-link">cors</li>
+                    <li className="footer-main-link">dotenv</li>
+                    <li className="footer-main-link">express</li>
+                    <li className="footer-main-link">mysql2</li>
+                </ul>
+                <ul className="footer-main-link-list">
+                    {/* More Projects */}
+                    <li className="footer-main-link">Film Library</li>
+                    <li className="footer-main-link">???</li>
+                    <li className="footer-main-link">???</li>
+                    <li className="footer-main-link">???</li>
+                </ul>
+            </section>
+            <section id="footer-sub-links">
+                <section id="footer-sub-links-left">
+                    <span>Privacy Policy</span>
+                    <span>Privacy Policy</span>
+                    <span>Privacy Policy</span>
+                </section>
+                <section id="footer-sub-links-right">
+                    <span>Privacy Policy</span>
+                    <span>Privacy Policy</span>
+                    <span>Privacy Policy</span>
+                </section>
+            </section>
+            <p id="footer-copyright">
                 &copy; { currentYear } Lyndon Mykal Panton |
                 All Rights Reserved
             </p>
