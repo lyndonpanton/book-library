dc8932b550cf793f5a3237471b6d83a7230594ea
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-13 22:25:26 +0100

    Removed unused code

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
 
-let faKitToken = process.env.FONTAWESOME_PACKAGE_TOKEN;
+// let faKitToken = process.env.FONTAWESOME_PACKAGE_TOKEN;
 
 function App() {
     return (
         <div className="App">
-            { console.log("Token: " + faKitToken) }
+            {/* { console.log("Token: " + faKitToken) } */}
             <script
                     src={"https://https://kit.fontawesome.com/" + faKitToken + ".js"}
                     crossOrigin="anonymous">
